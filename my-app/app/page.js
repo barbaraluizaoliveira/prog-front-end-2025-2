6cf5d5e14bc7bdc6ad0443d851f776ca0cbf15f2
Bárbara Luiza Oliveira <barbaraluizaoliveiradev@gmail.com>
2025-08-15 00:33:43 +0000

    add

--- my-app/app/page.js
+++ my-app/app/page.js
@@ -1,6 +1,13 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+import {Profile } from "@/components/profile"
 
 export default function Home() {
-  return <div>ola galeraaaaaaaaa</div>;
+  return (
+    <div>
+      <h1>olaaaaaaa</h1>
+      <Profile/>
+    </div>
+  )
 }
---
+++ my-app/components/profile.js
@@ -0,0 +1,9 @@
+export function Profile() {
+  return (
+    <img
+      src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Barbie_Logo.svg"
+      height="60%"
+      width="50%"
+    />
+  );
+}
